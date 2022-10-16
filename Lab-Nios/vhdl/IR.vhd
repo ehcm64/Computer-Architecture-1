9d690fa0717a67e3f4b7c89a04e1f69e4d5547f4
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-10-16 17:20:06 +0200

    Update IR.vhd

--- Lab-Nios/vhdl/IR.vhd
+++ Lab-Nios/vhdl/IR.vhd
@@ -12,7 +12,7 @@ end IR;
 
 architecture synth of IR is
 begin
-    flip_flop : process(clk, enable, D) is
+    flip_flop : process(clk) is
     begin
         if (rising_edge(clk) and enable = '1') then
             Q <= D;
