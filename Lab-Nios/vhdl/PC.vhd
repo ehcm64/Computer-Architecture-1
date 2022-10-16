016f314b7a5159dbb47042dfc92682453dc9ce0b
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2022-10-16 16:56:09 +0200

    Completed PC.vhd

--- Lab-Nios/vhdl/PC.vhd
+++ Lab-Nios/vhdl/PC.vhd
@@ -17,5 +17,28 @@ entity PC is
 end PC;
 
 architecture synth of PC is
+    signal s_addr : std_logic_vector(31 downto 0);
 begin
+
+    p_addr : process(reset_n, clk) is
+    begin
+        if (reset_n = '0') then
+            s_addr <= (others => '0');
+        elsif (rising_edge(clk)) then
+            if (en = '1') then
+                if (add_imm = '1') then
+                    s_addr <= std_logic_vector(signed(s_addr) + signed(imm));
+                elsif (sel_imm = '1') then
+                    s_addr <= "00000000000000" & imm & "00";
+                elsif (sel_a = '1') then
+                    s_addr <= "0000000000000000" & a;
+                else
+                    s_addr <= std_logic_vector(signed(s_addr) + 4);
+                end if;
+            end if;
+        end if;
+
+    end process p_addr;
+    addr <= s_addr;
+
 end synth;
